bab28a0cac8a5fcf24dd8e2434239e31b1c7a1a5
William Phan <william.da.phan@gmail.com>
2017-06-03 20:50:16 +0200

    Intial empty project (#2)

---
+++ app/src/main/java/io/github/fancyonion/exercisetimer/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.fancyonion.exercisetimer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
